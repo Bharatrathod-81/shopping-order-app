57208aa23183af6e1082a9b9d05eb4b8e8bf6e4e
Bharatrathod-81 <94593235+Bharatrathod-81@users.noreply.github.com>
2022-07-25 11:42:59 +0530

    fixed bug

--- src/services/order-data.jsx
+++ src/services/order-data.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const  OrdersList = async (dispatch) => {
     try{
-        const { data } = await axios.get("13.76.214.165:8001/api/orders?page=1&limit=15&order_status=",
+        const { data } = await axios.get("http://13.76.214.165:8001/api/orders?page=1&limit=15&order_status=",
         {
             headers : {Authorization:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOlsiYzQ5MGRmYTgtZWJmMy00NTE5LWI1M2EtZDc1Y2I3NGJlMDUwIiwiVXJ2aXNoIiwiU2hhaCIsInVydmlzaC5zaGFoQHB1c2hwYWsuYWkiXSwiaWF0IjoxNjQ5NzUyODc0LCJleHAiOjE2ODEyODg4NzR9.13UfXk_CVjKSqyC5pq2HgQK6KKI_PPM886C0dZB5CtM"}
         })
@@ -15,7 +15,7 @@ export const  OrdersList = async (dispatch) => {
 
 export const  OrdersSummary = async (dispatch) => {
     try{
-        const { data } = await axios.get("13.76.214.165:8001/api/analytics/summary",
+        const { data } = await axios.get("http://13.76.214.165:8001/api/analytics/summary",
         {
             headers : {Authorization:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOlsiYzQ5MGRmYTgtZWJmMy00NTE5LWI1M2EtZDc1Y2I3NGJlMDUwIiwiVXJ2aXNoIiwiU2hhaCIsInVydmlzaC5zaGFoQHB1c2hwYWsuYWkiXSwiaWF0IjoxNjQ5NzUyODc0LCJleHAiOjE2ODEyODg4NzR9.13UfXk_CVjKSqyC5pq2HgQK6KKI_PPM886C0dZB5CtM"}
         });
@@ -27,7 +27,7 @@ export const  OrdersSummary = async (dispatch) => {
 
 export const  OrdersLast7Days = async (dispatch) => {
     try{
-        const { data } = await axios.get("13.76.214.165:8001/api/analytics/last7Days",
+        const { data } = await axios.get("http://13.76.214.165:8001/api/analytics/last7Days",
         {
             headers : {Authorization:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOlsiYzQ5MGRmYTgtZWJmMy00NTE5LWI1M2EtZDc1Y2I3NGJlMDUwIiwiVXJ2aXNoIiwiU2hhaCIsInVydmlzaC5zaGFoQHB1c2hwYWsuYWkiXSwiaWF0IjoxNjQ5NzUyODc0LCJleHAiOjE2ODEyODg4NzR9.13UfXk_CVjKSqyC5pq2HgQK6KKI_PPM886C0dZB5CtM"}
         })
